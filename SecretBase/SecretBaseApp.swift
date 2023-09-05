22fccd72c5baea505bee277588521a71320f1c2b
Kawarai-S <kayawakakururu@icloud.com>
2023-09-05 17:50:01 +0900

    Initial Commit

---
+++ SecretBase/SecretBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecretBaseApp.swift
+//  SecretBase
+//
+//  Created by 瓦井つばさ on 2023/09/05.
+//
+
+import SwiftUI
+
+@main
+struct SecretBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
